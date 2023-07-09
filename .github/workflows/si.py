e73d403b1e6ae2a4eb8b65ec34b4655af75a56ce
kiet91 <139047344+kiet91@users.noreply.github.com>
2023-07-09 18:46:30 +0700

    Update si.py

--- .github/workflows/si.py
+++ .github/workflows/si.py
@@ -3,15 +3,14 @@ from prime_check import *
 
 def unit_check(n: int):
     f = fact(n).divisors()
-    if not prime_check(n):
-        for i in range(len(f) - 2):
-            if f[i + 2] + f[i + 1] % f[i] != 0:
+    if not prime_checkp(n):
+        for i in range(2, len(f)):
+            if f[i] + f[i - 1] % f[i - 2] == 0:
                 print(f'{n} is not the targeted integer')
                 break
-            if i == len(f) - 2:
+            if i == len(f) - 1:
                 print(f'{n} is the targeted integer')
     else:
         print(f'{n} is the targeted integer')
 
-
-unit_check(7) # nhap so nguyen duong bat ky
+unit_check(169) # nhap so nguyen duong bat ky
